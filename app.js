1d63f2fbf3b7b2e9d4f27c8eba7f0172e3f86171
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-05-24 22:51:06 -0700

    successfully setup get endpoint for tours

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 // REQUIRES //
 const express = require('express');
+const fs = require('fs');
+
 // INITIALIZE EXPRESS
 const app = express();
 
+// READ FILE FROM DATA FOLDER
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
 // ROUTES
 app.get('/api/v1/tours', (req, res) => {
-    
-})
+    res.status(200).json({
+        status: 'sucess',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+});
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: "Hello from the server side!", app: "Natours"});
