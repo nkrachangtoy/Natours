e1ae095c43a3c1f9714a8d429378d25d5215b1d5
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-05-25 15:19:56 -0700

    implement express router for tours and users routes

--- app.js
+++ app.js
@@ -170,28 +170,33 @@ const deleteUser = (req, res) => {
  * -------------------------------
  */
 
-app
-    .route('/api/v1/tours')
+const tourRouter = express.Router();
+const userRouter = express.Router(); 
+
+tourRouter
+    .route('/')
     .get(getAllTours)
     .post(createTour);
 
-app
-    .route('/api/v1/tours/:id')
+tourRouter
+    .route('/:id')
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
 
-app
-    .route('/api/v1/users')
+userRouter
+    .route('/')
     .get(getAllUsers)
     .post(createUser);
 
-app
-    .route('/api/v1/users/:id')
+userRouter
+    .route('/:id')
     .get(getUser)
     .patch(updateUser)
     .delete(deleteUser);
 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 /**
  * --------------------
  *      APP LISTEN
