bd0ee6318af69591c5aa0708b627a78db1725bfe
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-05-25 13:05:48 -0700

    Created get tour by id enpoint

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-// REQUIRES //
+/**
+ * ---------------------
+ *      REQUIRE
+ * ---------------------
+ */
 const express = require('express');
 const fs = require('fs');
 
 // INITIALIZE EXPRESS
 const app = express();
 
+/**
+ * ---------------------
+ *      MIDDLEWARES
+ * ---------------------
+ */
+app.use(express.json());
+
 // READ FILE FROM DATA FOLDER
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// ROUTES
+/**
+ * --------------------
+ *      ROUTES
+ * --------------------
+ */
+
+// Get All tours
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        status: 'sucess',
+        status: 'success',
         results: tours.length,
         data: {
             tours
@@ -21,10 +38,57 @@ app.get('/api/v1/tours', (req, res) => {
     })
 });
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({message: "Hello from the server side!", app: "Natours"});
-// });
+// Get tour by id
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
 
+    const id = req.params.id * 1;
+    const tour = tours.find(tour => tour.id === id);
+
+    // Handling invalid tour Id
+    if(id > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid Id'
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+});
+
+// Create new tour
+app.post('/api/v1/tours', (req,res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    // Object.assign allows us to create a new object by merging it to the existing file
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+    // Persist into the file
+    // Use writeFile() here because we can never block the event
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        // Send newly created as response
+        // status(201) means created
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
+
+
+/**
+ * --------------------
+ *      APP LISTEN
+ * --------------------
+ */
 const port = 8000;
 
 app.listen(port, ()=>{
