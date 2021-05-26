93d9b19c5a2d4d1197d581dcd2eaf1a743c2ce08
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-05-26 16:25:46 -0700

    connect to mongodb with mongoose and create tour schema

--- server.js
+++ server.js
@@ -4,17 +4,39 @@
  * -------------------------------
  */
 const dotenv = require('dotenv');
-// Read .env file
-dotenv.config({path: './config.env'})
-
-/**
- * -------------------------------
- *      REQUIRE
- * -------------------------------
- */
-
 const app = require('./app');
+const mongoose = require('mongoose');
 
+// Read .env file
+dotenv.config({path: './.env'})
+
+const db = process.env.MONGO_DB;
+
+// Mongoose connection
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {console.log('DB Connection successful!')});
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, ' A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 /**
  * -------------------------------
