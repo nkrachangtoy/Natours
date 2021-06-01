e1e06de7dc5dcce8d249aaca0f3fd57ae510513e
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-06-01 15:13:02 -0700

    implement filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,19 +14,26 @@ const Tour = require('./../models/tourModel');
 /**
  * Retrieve all tours
  * @GET
+ * @param {*} req.query returns matched tours with query
  * @returns Array of tours
  * @throws Will throw an error if no tours found
  */
 exports.getAllTours = async (req, res) => {
     try {
         // Build Query
+        // Filtering
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         
         // Loop through the array and delete words from the queryObj
         excludedFields.forEach(el => delete queryObj[el]);
         
-        const query = Tour.find(queryObj);
+        // Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr))
+
+        const query = Tour.find(JSON.parse(queryStr));
 
         // Execute Query
         // Get all tours with built-in method 'find()' from mongoose
