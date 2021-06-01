393430f9229bd56b98a1d6df56fbd00504999407
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-06-01 15:27:02 -0700

    implement sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,9 +31,16 @@ exports.getAllTours = async (req, res) => {
         // Advanced Filtering
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr))
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // Sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
 
         // Execute Query
         // Get all tours with built-in method 'find()' from mongoose
