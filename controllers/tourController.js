6def84400b5228e232022bbd78c725fb1b5ec166
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-06-01 16:12:55 -0700

    implement pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,20 +39,31 @@ exports.getAllTours = async (req, res) => {
             const sortBy = req.query.sort.split(',').join(' ');
             query = query.sort(sortBy);
         } else {
-            query = query.sort('-createdAt');
+            // when use $skip with $sort - _id is require as any duplicates can cause errors
+            query = query.sort('-createdAt _id');
         }
 
         // Field limiting
         if(req.query.fields){
             const fields = req.query.fields.split(',').join(' ');
-            query = query.select('name duration price');
-        }else{
+            query = query.select(fields);
+        } else {
             query = query.select('-__v');
         }
 
+        // Pagination
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page){
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('This page does not exist');
+        }
+
         // Execute Query
-        // Get all tours with built-in method 'find()' from mongoose
-        // This returns a promise   
         const tours = await query;
 
         res.status(200).json({
