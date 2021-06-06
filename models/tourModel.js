d002ff4a230d33b81c3734caebd8b8f7ef63ea6e
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-06-06 15:37:34 -0700

    add query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -63,7 +63,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, {
     toJSON: { virtuals: true},
     toObject: {virtuals: true}
@@ -91,6 +95,20 @@ tourSchema.pre('save', function(next){
 //     console.log(doc);
 //     next();
 // })
+
+// Query Middleware
+tourSchema.pre(/^find/, function(next){
+    this.find({secretTour: {$ne: true}});
+
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function(docs, next){
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`)
+    // console.log(docs);
+    next();
+});
 
 
 const Tour = mongoose.model('Tour', tourSchema);
